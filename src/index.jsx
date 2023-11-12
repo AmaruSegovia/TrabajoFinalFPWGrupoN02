ebd34e6bd63d2382940df88fb18c557c6e40454b
Ismael Ignacio Miguel Torres <ignacio25.it@gmail.com>
2023-11-12 02:29:32 -0300

    Primer Commit: Proyecto React iniciado

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+// Importando dependencias de React
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Importando otros componentes
+import App from "./App";
+
+// Inicio del código del componente
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <App></App>
+);
